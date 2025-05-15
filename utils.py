b19f18e35468829ae2a719e1fd576f9d83891cf0
Eshan Jayasundara <115539854+EshanJayasundara@users.noreply.github.com>
2025-05-15 16:26:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -43,6 +43,9 @@ def update_huggingface_dataset(new_data_df: pd.DataFrame, username:str, dataset_
         print(f"ℹ️ Could not load existing dataset. Starting fresh.")
         updated_df = new_data_df
 
+    if pd.Index(existing_df["question_id"]).intersection(pd.Index(new_data_df["question_id"])):
+        raise ValueError("Duplicate rows are not allowed. Please reconsider the date range.")
+
     # Convert final DataFrame to Hugging Face Dataset and push to hub
     if not updated_df.empty:
         dataset = Dataset.from_pandas(updated_df)
